5ba3c0b87ad0b2749ddf404c438257e060b96cf9
sk1404 <sheshadri1404@gmail.com>
2025-08-22 19:45:36 +0530

    mod9.v

--- mod9.v
+++ mod9.v
@@ -2,11 +2,11 @@ module mod9_counter (
     input clk, rst,
     output reg [3:0] count
 );
-    always @(posedge clk or posedge rst) begin
+    always @(posedge clk) begin
         if (rst) 
-            count <= 4'b0;
+            count <= 4'b0000;        // synchronous reset
         else if (count == 4'd8) 
-            count <= 4'b0;
+            count <= 4'b0000;        // wrap at 9
         else 
             count <= count + 1;
     end
