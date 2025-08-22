f9fc4a1944738db3b313add9ac291286ad65da36
sk1404 <sheshadri1404@gmail.com>
2025-08-22 19:44:06 +0530

    mod6.v

--- mod6.v
+++ mod6.v
@@ -2,9 +2,9 @@ module mod6_counter (
     input clk, rst,
     output reg [2:0] count
 );
-    always @(posedge clk or posedge rst) begin
+    always @(posedge clk) begin
         if (rst) 
-            count <= 3'b0;
+            count <= 3'b0;          // synchronous reset
         else if (count == 3'd5) 
             count <= 3'b0;
         else 
