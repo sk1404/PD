d66e73ca85b34a1bd5424584653a7b1f27dde1fd
sk1404 <sheshadri1404@gmail.com>
2025-08-22 19:38:48 +0530

    johnson.v

---
+++ johnson.v
@@ -0,0 +1,11 @@
+module johnson_counter (
+    input clk, rst,
+    output reg [3:0] out
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst) 
+            out <= 4'b0000;
+        else 
+            out <= {~out[0], out[3:1]};
+    end
+endmodule
